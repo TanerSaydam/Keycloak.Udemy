e913580c7b739917197d866e1aa07350611c138c
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-04 01:30:52 +0300

    Get all users

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebAPI.DTOs;
+using WebAPI.Options;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public sealed class UsersController(
+    KeycloakService keycloakService,
+    IOptions<KeycloakConfiguration> options) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        string enpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users";
+
+        var response = await keycloakService.GetAsync<List<UserDto>>(enpoint, true, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+}
