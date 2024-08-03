d4179c637e44dc080c28fb627c89cb0a713c4574
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-04 01:44:14 +0300

    Get users by params

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -22,4 +22,24 @@ public sealed class UsersController(
 
         return StatusCode(response.StatusCode, response);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByEmail(string email, CancellationToken cancellationToken)
+    {
+        string enpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users?email={email}";
+
+        var response = await keycloakService.GetAsync<List<UserDto>>(enpoint, true, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByUserName(string userName, CancellationToken cancellationToken)
+    {
+        string enpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users?username={userName}";
+
+        var response = await keycloakService.GetAsync<List<UserDto>>(enpoint, true, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
 }
