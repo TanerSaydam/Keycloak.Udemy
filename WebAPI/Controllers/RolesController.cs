263ba12d7957957592c57c7e6f37a18b5365bb8c
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-04 02:04:10 +0300

    Get all roles

---
+++ WebAPI/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebAPI.DTOs;
+using WebAPI.Options;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+//[Authorize]
+public sealed class RolesController(
+    KeycloakService keycloakService,
+    IOptions<KeycloakConfiguration> options) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        string enpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/clients/{options.Value.ClientUUID}/roles";
+
+        var response = await keycloakService.GetAsync<List<RoleDto>>(enpoint, true, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+}
