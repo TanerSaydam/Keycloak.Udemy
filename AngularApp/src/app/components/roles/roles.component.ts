63999aa5b7a76f1d2bd1c21f3f256cc49678b2c9
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-04 15:52:04 +0300

    Roles page, getall ve delete işlemi

--- AngularApp/src/app/components/roles/roles.component.ts
+++ AngularApp/src/app/components/roles/roles.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { FlexiToastService } from 'flexi-toast';
+import { FlexiGridModule } from 'flexi-grid';
+import { RoleModel } from '../../models/role.model';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FlexiGridModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+  roles = signal<RoleModel[]>([]);
 
+  constructor(
+    private http: HttpService,
+    private toast: FlexiToastService
+  ){
+    this.getAll();
+  }
+
+  getAll(){
+    this.http.get<RoleModel[]>("Roles/GetAll",(res)=> {
+      this.roles.set(res);
+    });
+  }
+
+  deleteById(name: string){
+    this.toast.showSwal("Delete Role?", "You want to delete this role?",()=> {
+      this.http.delete<string>(`Roles/DeleteByName?name=${name}`, res=> {
+        this.toast.showToast("Info",res, "info");
+        this.getAll();
+      });
+    })
+  }
 }
