87526f5865a733787e6f561e585fb648d5fdec7c
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-04 15:59:58 +0300

    Role create

--- AngularApp/src/app/components/roles/roles.component.ts
+++ AngularApp/src/app/components/roles/roles.component.ts
@@ -1,18 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { FlexiToastService } from 'flexi-toast';
 import { FlexiGridModule } from 'flexi-grid';
 import { RoleModel } from '../../models/role.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FlexiGridModule],
+  imports: [FlexiGridModule, FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
   roles = signal<RoleModel[]>([]);
+  name = signal<string>("");
+  description = signal<string>("");
+
+  @ViewChild("addModalCloseBtn") addModalCloseBtn: ElementRef<HTMLButtonElement> | undefined;
 
   constructor(
     private http: HttpService,
@@ -34,5 +39,15 @@ export class RolesComponent {
         this.getAll();
       });
     })
+  }
+
+  save(){
+    this.http.post<string>("Roles/Create", {name: this.name(), description: this.description()},(res)=> {
+      this.toast.showToast("Info",res, "info");
+      this.name.set("");
+      this.getAll();
+
+      this.addModalCloseBtn!.nativeElement.click();
+    });
   }
 }
